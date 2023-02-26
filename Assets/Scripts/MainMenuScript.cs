d55ff5ddf856342500fcac4dffff210fd81545da
Chil7 <chil7br0h@gmail.com>
2023-02-26 18:28:25 +0800

    1.2
    Implemented Basic Frame

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayButton()
+    {
+        Debug.Log("player has noe to choose new or continue");
+    }
+
+    public void GoToOptions()
+    {
+        Debug.Log("Player went to options");
+    }
+
+    public void QuitToDesktop()
+    {
+        Application.Quit();
+        Debug.Log("Player has exited to desktop");
+    }
+}
