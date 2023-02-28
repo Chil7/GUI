d4a92246b494133a34f5110a4af127df2e0d1e0a
Kyle Ben <chil7br0h@gmail.com>
2023-02-28 10:55:54 +0800

    1.8
    Title Implemented

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -5,14 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
     void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
     {
         
     }
