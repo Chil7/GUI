70132d42f022587c8015a980364e7fe0d46eae49
Kyle Ben <chil7br0h@gmail.com>
2023-03-07 11:29:25 +0800

    2.3
    Options Animation

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -2,24 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Animations;
 
 public class MainMenuScript : MonoBehaviour
 {
+    private Animator anim;
     
     void Start()
     {
+        anim = GetComponent<Animator>();
+    }
+
+    public void SwitchSceneAnim(string _sceneName)
+    {
         
+        anim.SetTrigger("SwitchScene");
+        StartCoroutine(SceneChange(_sceneName));
     }
 
     public void SwitchScene(string _sceneName)
     {
         SceneManager.LoadScene(_sceneName);
-        Debug.Log("Player went to options");
+        Debug.Log(_sceneName);
     }
 
     public void QuitToDesktop()
     {
         Application.Quit();
         Debug.Log("Player has exited to desktop");
+    }
+
+    IEnumerator SceneChange(string _sceneName)
+    {
+        float animLength = anim.GetCurrentAnimatorStateInfo(0).length;
+        while (animLength > 0)
+        {
+            yield return new WaitForSeconds(animLength);
+            SwitchScene(_sceneName);
+        }
+
     }
 }
--- Assets/Scripts/OptionScript.cs
+++ Assets/Scripts/OptionScript.cs
@@ -4,24 +4,53 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using UnityEngine.Audio;
+using UnityEngine.Animations;
 
 
 public class OptionScript : MonoBehaviour
 {
+    private Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SwitchSceneAnim(string _sceneName)
     {
-        
+
+        anim.SetTrigger("SwitchScene");
+        StartCoroutine(SceneChange(_sceneName));
     }
 
     public void SwitchScene(string _sceneName)
     {
         SceneManager.LoadScene(_sceneName);
+        Debug.Log(_sceneName);
+    }
+
+    IEnumerator SceneChange(string _sceneName)
+    {
+        float animLength = anim.GetCurrentAnimatorStateInfo(0).length;
+        while (animLength > 0)
+        {
+            yield return new WaitForSeconds(animLength);
+            SwitchScene(_sceneName);
+        }
+
+    }
+
+    public void GlitchyArt(string _bool)
+    {
+        if(_bool == "true")
+        {
+            anim.SetBool("GlitchArt", true);
+        }
+        else if (_bool == "false")
+        {
+            anim.SetBool("GlitchArt", false);
+        }
+
     }
 }
